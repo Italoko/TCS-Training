15bd4cfedb4364ee1d7d72a1dffa95b30509a9f7
ITALO PIOVAN <italo_piovan@hotmail.com>
2022-01-21 22:26:12 -0300

    Data access layer implementation

--- avaliacao_tcs/src/java/DAO/DAOCliente.java
+++ avaliacao_tcs/src/java/DAO/DAOCliente.java
@@ -1,9 +1,103 @@
 package DAO;
+import MODEL.Cliente;
+import java.util.ArrayList;
+import java.sql.ResultSet;
 
 /**
  *
  * @author ITALO PIOVAN
  */
-public class DAOCliente {
+public class DAOCliente 
+{
+    private static DAOCliente instance;
+    private Conexao conn = new Conexao();
     
+    private DAOCliente() {}
+    
+    public static synchronized DAOCliente getInstance() 
+    {
+        if (instance == null)
+            instance = new DAOCliente();
+        return instance;
+    }
+    
+    public void insertCliente(Cliente cliente)
+    {
+        String sql = "INSERT INTO tb_cliente (nome_cliente, email_cliente, telefone_cliente) VALUES ('@a', '@b', '@c')";
+        
+        sql = sql.replace("@a", cliente.getNome_cliente());
+        sql = sql.replace("@b", cliente.getEmail_cliente());
+        sql = sql.replace("@c", cliente.getTelefone_cliente());
+        
+        conn.openConnection();
+        conn.manipulate(sql);
+        conn.closeConnection();
+    }
+    
+    public boolean updateCliente(Cliente cliente)
+    {
+        boolean success = false;
+        String sql = "UPDATE tb_cliente SET nome_cliente = '@a', email_cliente = '@b', telefone_cliente = '@c' WHERE id_cliente = @d";
+        
+        sql = sql.replace("@a", cliente.getNome_cliente());
+        sql = sql.replace("@b", cliente.getEmail_cliente());
+        sql = sql.replace("@c", cliente.getTelefone_cliente());
+        sql = sql.replace("@d", Integer.toString(cliente.getId_cliente()));
+        
+        conn.openConnection();
+        success = conn.manipulate(sql);
+        conn.closeConnection();
+        return success;
+    }
+    
+    public boolean deleteCliente(int id)
+    {
+        boolean success = false;
+        String sql = "DELETE FROM tb_cliente WHERE id_cliente = @a";
+   
+        sql = sql.replace("@a", Integer.toString(id));
+        
+        conn.openConnection();
+        success = conn.manipulate(sql);
+        conn.closeConnection();
+        return success;
+    }
+    
+    public ArrayList<Cliente> getClientes()
+    {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        try{
+            String sql = "SELECT * FROM tb_cliente";
+            
+            conn.openConnection();
+            ResultSet rs = conn.query(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    clientes.add(new Cliente(rs.getInt("id_cliente"), rs.getString("nome_cliente"), 
+                                             rs.getString("email_cliente"), rs.getString("telefone_cliente")));
+            
+            conn.closeConnection();   
+        }catch(Exception e){throw new RuntimeException("Erro ao listar clientes" + e);}
+        return clientes;
+    }
+    
+    public ArrayList<Cliente> getClientesFilter(String nome)
+    {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        try{
+            String sql = "SELECT * FROM tb_cliente WHERE nome_cliente LIKE '%" +nome+ "%'";
+            
+            conn.openConnection();
+            ResultSet rs = conn.query(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    clientes.add(new Cliente(rs.getInt("id_cliente"), rs.getString("nome_cliente"), 
+                                             rs.getString("email_cliente"), rs.getString("telefone_cliente")));
+            
+            conn.closeConnection();
+        }catch(Exception e){throw new RuntimeException("Erro ao listar clientes" + e);}
+        return clientes;
+    }
 }
--- avaliacao_tcs/src/java/DAO/DAOProduto.java
+++ avaliacao_tcs/src/java/DAO/DAOProduto.java
@@ -1,9 +1,103 @@
 package DAO;
 
+import MODEL.Produto;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author ITALO PIOVAN
  */
-public class DAOProduto {
+public class DAOProduto 
+{
+    private static DAOProduto instance;
+    private Conexao conn = new Conexao();
     
+    private DAOProduto() {}
+    
+    public static synchronized DAOProduto getInstance() 
+    {
+        if (instance == null)
+            instance = new DAOProduto();
+        return instance;
+    }
+    
+    public boolean insertProduto(Produto produto)
+    {
+        boolean success = false;
+        String sql = "INSERT INTO tb_produto (nome_produto, qtd_produto) VALUES ('@a', '@b')";
+        
+        sql = sql.replace("@a", produto.getNome_produto());
+        sql = sql.replace("@b", Integer.toString(produto.getQtd_produto()));
+        
+        conn.openConnection();
+        success = conn.manipulate(sql);
+        conn.closeConnection();
+        
+        return success;
+    }
+    
+    public boolean updateProduto(Produto produto)
+    {
+        boolean success = false;
+        String sql = "UPDATE tb_produto SET nome_produto = '@a', qtd_produto = '@b' WHERE id_produto = @c";
+        
+        sql = sql.replace("@a", produto.getNome_produto());
+        sql = sql.replace("@b", Integer.toString(produto.getQtd_produto()));
+        sql = sql.replace("@c", Integer.toString(produto.getId_produto()));
+        
+        conn.openConnection();
+        success = conn.manipulate(sql);
+        conn.closeConnection();
+        return success;
+    }
+    
+    public boolean deleteProduto(int id)
+    {
+        boolean success = false;
+        String sql = "DELETE FROM tb_produto WHERE id_produto = @a";
+   
+        sql = sql.replace("@a", Integer.toString(id));
+        
+        conn.openConnection();
+        success = conn.manipulate(sql);
+        conn.closeConnection();
+        return success;
+    }
+    
+    public ArrayList<Produto> getProdutos()
+    {
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        try{
+            String sql = "SELECT * FROM tb_produto";
+            
+            conn.openConnection();
+            ResultSet rs = conn.query(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    produtos.add(new Produto(rs.getInt("id_produto"), rs.getString("nome_produto"), rs.getInt("qtd_produto")));
+            
+            conn.closeConnection();   
+        }catch(Exception e){throw new RuntimeException("Erro ao listar produtos" + e);}
+        return produtos;
+    }
+    
+    public ArrayList<Produto> getProdutosFilter(String nome)
+    {
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        try{
+            String sql = "SELECT * FROM tb_produto WHERE nome_produto LIKE '%" +nome+ "%'";
+            
+            conn.openConnection();
+            ResultSet rs = conn.query(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    produtos.add(new Produto(rs.getInt("id_produto"), rs.getString("nome_produto"), rs.getInt("qtd_produto")));
+                                            
+            conn.closeConnection();
+        }catch(Exception e){throw new RuntimeException("Erro ao listar produtos" + e);}
+        return produtos;
+    }
 }
